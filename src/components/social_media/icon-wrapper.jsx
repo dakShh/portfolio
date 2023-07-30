8b480ee00cefd373b8325e48c8dfb3e4c0fc58d2
dakShh <daksh.dk207@gmail.com>
2023-07-30 14:13:44 +0530

    socials are now functionable

--- src/components/social_media/icon-wrapper.jsx
+++ src/components/social_media/icon-wrapper.jsx
@@ -1,22 +1,53 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useState } from "react";
 
-const IconWrapper = ({ children, icon, delay }) => {
+const IconWrapper = ({ children, icon, delay, content, link }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleOpen(state) {
+    setIsOpen(state);
+  }
   return (
-    <div
-      data-aos="zoom-in-up"
-      data-aos-delay={delay ?? "200"}
-      className={clsx(
-        "p-3",
-        "w-[40px] h-[40px] lg:w-[50px] lg:h-[50px]",
-        "rounded-full shadow-md relative  shadow-black/50",
-        "flex justify-center items-center",
-        "backdrop-blur-sm",
-        "bg-white/10"
-      )}
-    >
-      <i className={clsx("absolute", "text-xl lg:text-2xl font-base", "text-white", `${icon}`)}></i>
-    </div>
+    <a target="_blank" href={`${link}`}>
+      <div
+        data-aos="zoom-in-up"
+        data-aos-delay={delay ?? "200"}
+        className={clsx(
+          "group relative p-3",
+          "w-[40px] h-[40px] lg:w-[50px] lg:h-[50px]",
+          "rounded-full shadow-md relative  shadow-black/50",
+          "flex justify-center items-center",
+          "backdrop-blur-sm",
+          "bg-white/10",
+          "cursor-pointer",
+          "hover:bg-white/75"
+        )}
+        onMouseEnter={() => toggleOpen(true)}
+        onMouseLeave={() => toggleOpen(false)}
+      >
+        <div
+          className={clsx(
+            "absolute right-[60px]",
+            `${isOpen ? "opacity-[1]" : "opacity-[0]"}`,
+            "transition duration-500",
+            `${isOpen ? "translate-x-0" : "translate-x-8"}`,
+            "bg-white/20 backdrop-blur-[3px] rounded-lg px-2 py-1 font-bold ",
+            "text-white",
+            "min-w-[140px]"
+          )}
+        >
+          {content}
+        </div>
+        <i
+          className={clsx(
+            "absolute",
+            "text-xl lg:text-2xl font-base",
+            "text-white group-hover:text-black",
+            `${icon}`
+          )}
+        ></i>
+      </div>
+    </a>
   );
 };
 
--- src/components/social_media/socials.jsx
+++ src/components/social_media/socials.jsx
@@ -6,11 +6,36 @@ const Socials = () => {
   return (
     <div className={clsx("fixed", "right-[30px] top-[35px] lg:right-[50px] lg:top-[50px]")}>
       <div className={clsx("flex flex-col gap-y-5")}>
-        <IconWrapper delay={200} icon="fa-regular fa-envelope" />
-        <IconWrapper delay={300} icon="fa-brands fa-whatsapp" />
-        <IconWrapper delay={400} icon="fa-brands fa-facebook-f" />
-        <IconWrapper delay={500} icon="fa-brands fa-github" />
-        <IconWrapper delay={600} icon="fa-brands fa-linkedin-in" />
+        <IconWrapper
+          delay={200}
+          icon="fa-regular fa-envelope"
+          content={"daksh.dk207@gmail.com"}
+          link={"mailto:daksh.dk207@gmail.com"}
+        />
+        <IconWrapper
+          delay={300}
+          icon="fa-brands fa-whatsapp"
+          content={"+91 9769104428"}
+          link={"https://api.whatsapp.com/send?phone=9769104428&text=Hello"}
+        />
+        <IconWrapper
+          delay={400}
+          icon="fa-brands fa-facebook-f"
+          content={"https://www.facebook.com/daksh.khatri/"}
+          link={"https://www.facebook.com/daksh.khatri/"}
+        />
+        <IconWrapper
+          delay={500}
+          icon="fa-brands fa-github"
+          content={"https://github.com/dakShh"}
+          link={"https://github.com/dakShh"}
+        />
+        <IconWrapper
+          delay={600}
+          icon="fa-brands fa-linkedin-in"
+          content={"https://www.linkedin.com/in/daksh-khatri/"}
+          link={"https://www.linkedin.com/in/daksh-khatri/"}
+        />
       </div>
     </div>
   );
